8a426330faf464f755f10fef49579186a378a775
Egor Blyablin <eg.blyablin@yandex.ru>
2024-03-08 23:11:17 +0300

    Создание пустого приложения

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
-int main(int argc, char const *argv[])
+#include <SFML/Graphics.hpp>
+
+int main()
 {
-    /* code */
+    sf::RenderWindow window(sf::VideoMode(640, 480), "RTetris");
+
+    while (window.isOpen()) // цикл отрисовки
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(); // очистка кадра
+
+        // отрисовка следующего кадра
+
+        window.display(); // показ кадра
+    }
+
     return 0;
 }
