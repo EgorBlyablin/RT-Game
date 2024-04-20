5f839c549a0cf601f6bc99c40e78b1f61b222260
Egor Blyablin <eg.blyablin@yandex.ru>
2024-04-20 23:21:04 +0300

    Создан класс Game, который контролирует всю работу игры

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,9 @@
-#include <iostream>
-#include <math.h>
-
-#include <imgui.h>
-#include <imgui-SFML.h>
-#include <SFML/Graphics.hpp>
+#include "Game.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "RTetris");
-    ImGui::SFML::Init(window);
-
-    sf::Clock deltaClock;
-    while (window.isOpen()) // цикл отрисовки
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            ImGui::SFML::ProcessEvent(window, event);
-
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-        ImGui::ShowMetricsWindow();
-        ImGui::ShowDebugLogWindow();
-
-        window.clear(); // очистка кадра
-        ImGui::SFML::Render(window);
-        window.display(); // показ кадра
-    }
+    Game::init();
+    Game::run();
 
     return 0;
 }
