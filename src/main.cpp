866eabd13a51ceb4d8fdcd38f56456da4b3e1af0
Egor Blyablin <eg.blyablin@yandex.ru>
2024-04-20 13:22:43 +0300

    Полный откат и переход на ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,32 @@
-#include <fstream>
 #include <iostream>
-#include <math.h>
 
+#include <imgui.h>
+#include <imgui-SFML.h>
 #include <SFML/Graphics.hpp>
-
-#include "Utilities/DebugInfo.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(320, 240), "RTetris");
-    DebugInfo debugInfo;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "RTetris");
+    ImGui::SFML::Init(window);
 
+    sf::Clock deltaClock;
     while (window.isOpen()) // цикл отрисовки
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+            ImGui::SFML::ProcessEvent(window, event);
+
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+        ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::ShowMetricsWindow();
+        ImGui::ShowDebugLogWindow();
+
         window.clear(); // очистка кадра
-
-        debugInfo.update();
-        window.draw(debugInfo); // отрисовка следующего кадра
-
+        ImGui::SFML::Render(window);
         window.display(); // показ кадра
     }
 
