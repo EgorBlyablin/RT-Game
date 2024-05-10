d8f582e9d05fb7acca60a72ab8eff3154452c3d4
Egor Blyablin <eg.blyablin@yandex.ru>
2024-05-10 16:19:10 +0300

    Удаление мусора

--- src/Screen/Game.cpp
+++ src/Screen/Game.cpp
@@ -6,8 +6,6 @@
 Game::Game(sf::Vector2f windowSize)
     : camera(sf::Vector2f(MAPSIZE * TILE_SIZE_PX, MAPSIZE * TILE_SIZE_PX), 1.f, 1.f, 10.f, 1.1f, windowSize)
 {
-
-    // characters[0]->moveTo(sf::Vector2u(10, 5), this);
 }
 
 void Game::handleEvent(const sf::Event &event) // обработка событий игры
