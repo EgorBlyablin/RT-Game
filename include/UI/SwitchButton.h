22e1dac52b1d9149f0de538af155f927d1ebfe8a
Иван <ifef228@gmail.com>
2024-05-11 12:59:39 +0300

    доделал кнопку с выбором

---
+++ include/UI/SwitchButton.h
@@ -0,0 +1,53 @@
+//
+// Created by Иван on 04.05.2024.
+//
+
+#ifndef RTETRIS_SWITCHBUTTON_H
+#define RTETRIS_SWITCHBUTTON_H
+
+#include <map>
+
+#include "Button.h"
+
+
+/// @brief Класс кнопки c выбором
+/// @details Выполняет отрисовку кнопки с двумя стрелочками справа и слева в соответствии с ее состоянием,
+/// изменяет ее состояние в соответствии с событиями,
+/// вызывает callback-функцию
+///@todo не прокручивается выбор, прога вылетает с исключением, когда выбор должен перекрутиться в начало/конец
+class SwitchButton :public Button
+{
+  protected:
+    std::map<std::string, std::function<void(void)>> callbacks;
+    std::map<std::string, std::function<void(void)>>::iterator choice;
+
+
+
+    sf::RectangleShape info;//информация о выбранной настройке
+    sf::Text infoText;
+
+    Button left;
+    Button right;
+
+
+  public:
+    SwitchButton()= default;
+    SwitchButton(const sf::Vector2f &position, const sf::Vector2f &size, const std::string &text, const sf::Font &font,
+                          const unsigned int characterSize,const std::map<std::string, std::function<void(void)>> &callbacks);
+    SwitchButton(const sf::Vector2f &size, const std::string &text, const sf::Font &font,
+                 const unsigned int characterSize,const std::map<std::string, std::function<void(void)>> &callbacks): SwitchButton(sf::Vector2f(0,0),size,text, font, characterSize, callbacks){}
+
+    void updateColor() override;
+
+    void setPosition(sf::Vector2f position) override;
+
+    void handleEvent(const sf::Event &event) override;
+
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+    void apply();
+
+    friend Button;
+};
+
+#endif // RTETRIS_SWITCHBUTTON_H
---
+++ src/UI/SwitchButton.cpp
@@ -0,0 +1,107 @@
+//
+// Created by Иван on 04.05.2024.
+//
+
+#include <iostream>
+
+#include "Application.h"
+#include "UI/SwitchButton.h"
+
+SwitchButton::SwitchButton(const sf::Vector2f &position, const sf::Vector2f &size, const std::string &text, const sf::Font &font,
+                           const unsigned int characterSize,const std::map<std::string, std::function<void(void)>> &callbacks) : callbacks(callbacks)
+{
+
+    this->choice = this->callbacks.begin();
+
+
+    shape.setSize(size); // установка размеров заднего фона
+
+    this->text.setString(text); // установка текста названия кнопки
+    this->text.setFont(font);
+    this->text.setCharacterSize(characterSize); // установка размера текста
+
+    info.setSize(sf::Vector2f(80,40));
+
+    this->infoText.setString(this->callbacks.begin()->first);
+    this->infoText.setFont(font);
+    this->infoText.setCharacterSize(characterSize-3);
+
+
+    info.setPosition(sf::Vector2f(position.x+size.x/2+26, position.y)); // позиционирование заднего фона(делается здесь для учета заданного размера кнопки)
+
+    setPosition(position); // позиционирование названия кнопки
+
+
+    ///левая и правая кнопки перемещения и кнопка подтверждения
+    left = Button(sf::Vector2f(position.x+size.x/2+3,position.y), sf::Vector2f(20,20), "<-", font, 20,
+                  [this]{
+                      //итератор должен декрементироваться, либо же перемещаться в конец контейнера, если достигнуто начало
+                      if (this->choice == this->callbacks.begin()){
+                          this->choice = std::next(this->callbacks.end(), -1);
+                      }
+                      else{
+                          this->choice = std::next(this->choice, -1);
+                      }
+                  });
+
+
+
+
+    right = Button(sf::Vector2f(position.x+size.x/2+3 +83 ,position.y), sf::Vector2f(20,20), "->", font, 20,
+                   [this]{
+                       //итератор должен инкрементироваться, либо же перемещаться в начало контейнера, если достигнут конец
+                       this->choice = (this->choice == std::next(this->callbacks.end(), -1))? this->callbacks.begin(): std::next(this->choice, 1);
+                   });
+
+
+}
+void SwitchButton::updateColor()
+{
+    shape.setFillColor(backgroundColor);
+    text.setFillColor(textColor);
+
+    info.setFillColor(backgroundColor);
+    infoText.setFillColor(textColor);
+
+    infoText.setString(choice->first);
+}
+void SwitchButton::handleEvent(const sf::Event &event)
+{
+    left.handleEvent(event);
+    right.handleEvent(event);
+
+
+    updateColor();
+}
+void SwitchButton::setPosition(sf::Vector2f position)
+{
+    Button::setPosition(position);
+
+
+
+
+    infoText.setOrigin(
+        infoText.getGlobalBounds().left,
+        infoText.getGlobalBounds().top); // обновление исходной точки текста (т.к. размеры текста могли измениться)
+    infoText.setPosition(info.getPosition() +
+                     (info.getSize() - sf::Vector2f(infoText.getGlobalBounds().width, infoText.getGlobalBounds().height)) /
+                         2.f); // центрирование текста на кнопке
+}
+void SwitchButton::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+
+    Button::draw(target, states);
+    left.draw(target, states);
+    right.draw(target, states);
+
+
+    target.draw(info);
+    target.draw(infoText);
+
+}
+
+void SwitchButton::apply()
+{
+    this->choice->second();
+}
