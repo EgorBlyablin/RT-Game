e3a755df6b2d35b118224e36e8a8585b4c4b6b38
Egor Blyablin <60047296+EgorkA82@users.noreply.github.com>
2024-05-20 14:45:46 +0300

    Добавлено отображение полоски здоровья над юнитом (#26)
    Добавлена отображение полоски здоровья над юнитом, если его здоровье меньше максимального

--- src/Gameplay/Units/BaseUnit.cpp
+++ src/Gameplay/Units/BaseUnit.cpp
@@ -2,6 +2,8 @@
 
 #include "Gameplay/Units/BaseUnit.h"
 
+#define HP_BAR_OUTLINE 0.5f
+
 BaseUnit::BaseUnit(sf::Vector2u position, unsigned int hp, unsigned int maxHP, const sf::Texture &texture,
                    sf::IntRect area)
     : position(position), hp(hp), maxHP(maxHP), texture(texture), area(area)
@@ -52,7 +54,27 @@ void BaseUnit::draw(sf::RenderTarget &target, sf::RenderStates states) const
     states.transform *= getTransform();
 
     sf::Sprite sprite(texture, area);
+    sprite.setScale(1 - 2.f / sprite.getGlobalBounds().width, 1 - 2.f / sprite.getGlobalBounds().height);
+    sprite.move(0.5f, 0.5f);
     target.draw(sprite, states);
+
+    if (hp < maxHP)
+    {
+        sf::RectangleShape background(
+            sf::Vector2f(sprite.getGlobalBounds().width, sprite.getGlobalBounds().height / 8));
+        background.setFillColor(sf::Color::Black);
+
+        float hpRatio = (float)hp / maxHP;
+        sf::RectangleShape bar(sf::Vector2f(background.getGlobalBounds().width * hpRatio - HP_BAR_OUTLINE * 2,
+                                            background.getGlobalBounds().height - HP_BAR_OUTLINE * 2));
+        bar.setFillColor(sf::Color::Green);
+
+        background.setPosition(sprite.getGlobalBounds().left, sprite.getGlobalBounds().top);
+        bar.setPosition(background.getPosition() + sf::Vector2f(HP_BAR_OUTLINE, HP_BAR_OUTLINE));
+
+        target.draw(background, states);
+        target.draw(bar, states);
+    }
 }
 
 unsigned int BaseUnit::distance(sf::Vector2u first, sf::Vector2u second)
